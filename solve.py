c37fd2ac294007240ab048fef995028f62b1c1a8
dhysun <davidhongyusun@gmail.com>
2026-08-17 22:17:20 -0400

    print maze sol

--- solve.py
+++ solve.py
@@ -12,3 +12,29 @@ def build_actor(num_actions):
         nn.Tanh(),
         nn.LazyLinear(num_actions)
     )
+
+def print_maze_solution(maze, path, start, goal):
+    res = []
+
+    for y in range(maze.shape[0]):
+        row = []
+
+        for x in range(maze.shape[1]):
+            if maze[y, x] == 1:
+                row.append("#")
+            else:
+                row.append(" ")
+        
+        res.append(row)
+
+    for (y, x) in path:
+        res[y][x] = "."
+    
+    start_y, start_x = start
+    goal_y, goal_x = goal
+
+    res[start_y][start_x] = "S"
+    res[goal_y][goal_x] = "E"
+
+    for row in res:
+        print("".join(row))
